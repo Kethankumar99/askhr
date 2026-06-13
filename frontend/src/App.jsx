1cd5e3e1aa412c488928d4237f48869b7647b0b9
Kethankumar99 <cevurukethankumar99@gmail.com>
2026-06-13 17:26:46 +0530

    Set default route to login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import EmployeeChat from './pages/EmployeeChat';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HRLogin from './pages/HRLogin';
 import HRRegister from './pages/HRRegister';
 import HRDashboard from './pages/HRDashboard';
+import EmployeeChat from './pages/EmployeeChat';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Employee Chat */}
-        <Route path="/chat/:company" element={<EmployeeChat />} />
-        
-        {/* HR Pages */}
+        <Route path="/" element={<Navigate to="/hr/login" />} />
         <Route path="/hr/login" element={<HRLogin />} />
         <Route path="/hr/register" element={<HRRegister />} />
         <Route path="/hr/dashboard" element={<HRDashboard />} />
+        <Route path="/chat/:company" element={<EmployeeChat />} />
       </Routes>
     </Router>
   );
